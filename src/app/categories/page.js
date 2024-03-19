2e82aa39a78c2e02801e2be65b55847b26257fb3
Totogodo <mister.kol9@gmail.com>
2024-03-20 00:21:50 +0100

    Create categori front part (API request and form) - done

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -1,9 +1,31 @@
 "use client";
 import UserTabs from "@/components/layout/UserTabs";
 import { useProfile } from "@/components/UseProfile";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function CategoriesPage() {
   const { loading: profileLoading, data: profileData } = useProfile();
+  const [newCategoryName, setCategoryName] = useState();
+
+  async function handleNewCategorySubmit(ev) {
+    ev.preventDefault();
+    const creationPromise = new Promise(async (resolve, reject) => {
+      const response = await fetch("/api/categories", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: newCategoryName }),
+      });
+
+      response.ok ? resolve() : reject();
+    });
+
+    toast.promise(creationPromise, {
+      loading: "Creating new category...",
+      success: "Category created!",
+      error: "Can`t create category",
+    });
+  }
 
   if (profileLoading) {
     return "Loading data...";
@@ -15,7 +37,7 @@ export default function CategoriesPage() {
   return (
     <section className="mt-8 max-w-lg mx-auto">
       <UserTabs isAdmin={profileData.admin} />
-      <form className="mt-8">
+      <form className="mt-8" onSubmit={handleNewCategorySubmit}>
         <div className="flex gap-2 items-end">
           <div className="grow">
             <label>New Category Name</label>
